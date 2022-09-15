0d0e3da4e794fea26d807a59b33fa8a6debf1c38
aria hidayat <ariamustofa@gmail.com>
2022-09-16 03:04:10 +0700

    feat: Spatie Setup

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('admin', function () {
+    return 'Admin Page';
+})->middleware('role:admin');
+
+Route::get('user', function () {
+    return 'User Page';
+})->middleware('role:user');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
